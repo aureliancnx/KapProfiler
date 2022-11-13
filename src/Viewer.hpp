c8436947b6048aee6dd7db1eb5a29bdddd57d15d
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-13 18:37:57 +0100

    add: viewer system

---
+++ src/Viewer.hpp
@@ -0,0 +1,30 @@
+#ifndef PROFILER_GLOBIMPL_HPP
+#define PROFILER_GLOBIMPL_HPP
+
+#include <glob.h>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <sstream>
+
+namespace KapEngine {
+
+    namespace Profiler {
+
+        class Viewer {
+
+          private:
+            std::string _filePattern;
+
+          public:
+            Viewer(std::string &filePattern);
+            ~Viewer();
+
+            void initializeWindow();
+            void run();
+        };
+
+    } // namespace Profiler
+} // namespace KapEngine
+
+#endif // PROFILER_GLOBIMPL_HPP
