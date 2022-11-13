5016827b26a51a842e9213684ad129a87291f4d2
aureliancnx <aurelien.marcel@epitech.eu>
2022-11-13 18:41:17 +0100

    CHG: Viewer - starting implementation with viewer system

--- src/Viewer.cpp
+++ src/Viewer.cpp
@@ -1,1 +1,44 @@
 #include "Viewer.hpp"
+
+using namespace KapEngine::Profiler;
+
+Viewer::Viewer(std::string &filePattern) {
+    _filePattern = filePattern;
+}
+
+void Viewer::initializeWindow() {
+    // Window is already initialized
+    if (_windowInitialized) {
+        return;
+    }
+    _windowInitialized = true;
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+        return 1;
+    }
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "KapProfiler", NULL, NULL);
+    if (window == NULL) {
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL2_Init();
+}
+
+void Viewer::errorCallback(int error, const char *content) {
+    fprintf(stderr, "Error with GLFW - ID %d: %s\n", error, content);
+}
+
+
+void Viewer::run() {
+
+}
